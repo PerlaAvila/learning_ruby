ac782848f20f2e5a9a829215dff6439450c8cbd0
phacohen <perla.avila.amaro@gmail.com>
2016-11-06 15:32:57 -0600

    Add files via upload

--- preparacion-1/jueves/values.rb
+++ preparacion-1/jueves/values.rb
@@ -1,7 +1,9 @@
 hash = { dato_1: 'bla', dato_2: 'bla_2', dato_3: 'bla_3', dato_4: 'bla_4' }
 
 def hw(hash)
-  hash.values
+ values = Array.new
+  hash.each do |k, v|
+  	values << v
 end
 
 p hw(hash) == ["bla", "bla_2", "bla_3", "bla_4"]
