ea1186321cdb3876bfc985c056751020fe36463b
phacohen <perla.avila.amaro@gmail.com>
2016-11-07 19:20:05 -0600

    Add files via upload

--- preparacion-1/jueves/hash.rb
+++ preparacion-1/jueves/hash.rb
@@ -1,9 +1,6 @@
 new_hash = Hash.new
-canasta_de_frutas = Hash.new
-canasta_de_frutas[:papaya] = 3
-canasta_de_frutas[:platano] = 4
-canasta_de_frutas[:mango] = 6
-
-canasta_de_frutas[:fresa] = 10
-
-fruta_seleccionada = 6
+  canasta_de_frutas = {"papaya" => 3, "naranja" => 4, "uva" => 12}
+  canasta_de_frutas["fresa"] = 10
+p canasta_de_frutas
+  fruta_seleccionada = canasta_de_frutas.values_at("uva")
+p fruta_seleccionada
--- preparacion-1/jueves/keys.rb
+++ preparacion-1/jueves/keys.rb
@@ -1,11 +1,6 @@
 hash = { dato_1: 'bla', dato_2: 'bla_2', dato_3: 'bla_3', dato_4: 'bla_4' }
-
 def hw(hash)
- keys = Array.new
-  hash.each do |k, v|
-  	keys << k
-  end
-  keys
+  hash.each_key.to_a
 end
 
 p hw(hash) == [:dato_1, :dato_2, :dato_3, :dato_4]
